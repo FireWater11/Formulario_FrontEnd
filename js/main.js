331993e67300d62f26767e4330d6e6cf5923f23c
Luidi Augusto <luidi.augusto1234@gmail.com>
2025-08-28 15:26:05 -0300

    fix: corrigindo comentarios

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const inputSenha = document.getElementById("senha"); // input
   const botaoOlho = document.getElementById("alternarSenha"); // button
-  const textoAlterar = document.getElementById("senhaErrorText");// label
+  const textoAlterar = document.getElementById("senhaErrorText"); // label
 
   const msgErroLabel = document.getElementById("emailTexto"); // label
   const inputEmail = document.getElementById("email"); // input
@@ -10,51 +10,49 @@ document.addEventListener("DOMContentLoaded", () => {
   const imgMostrar = document.getElementById("imgCheck"); // img
 
   botaoOlho.addEventListener("click", () => {
+
     if (inputSenha.type === "password") {
       inputSenha.type = "text";
-    } else {
+    } 
+    
+    else {
       inputSenha.type = "password";
     }
   });
 
   inputSenha.addEventListener("input", () => {
-
     if (inputSenha.value === "") {
       inputSenha.style.border = "2px solid red";
       inputSenha.style.borderRadius = "10px";
-      inputSenha.style.width = "";
       textoAlterar.style.color = "red";
-
-    } else {
+    } 
+    
+    else {
       inputSenha.style.border = "";
       textoAlterar.style.color = "black";
     }
   });
 
   inputEmail.addEventListener("input", () => {
-
     if (inputEmail.value === "") {
       inputEmail.style.border = "2px solid red";
       inputEmail.style.borderRadius = "10px";
-      inputEmail.style.width = "";
       msgErroLabel.style.color = "red";
-    } else {
-        inputEmail.style.border = "black";
-        msgErroLabel.style.color = "black";
+    } 
+    
+    else {
+      inputEmail.style.border = "";
+      msgErroLabel.style.color = "black";
     }
-  })
+  });
 
   imgMostrar.style.display = "none";
-
   inputCheckbox.addEventListener("click", () => {
     if (inputCheckbox.checked) {
-      imgMostrar.style.display = "block"
-    } else {
-      imgMostrar.style.display = "none"
+      imgMostrar.style.display = "block";
+    } 
+    else {
+      imgMostrar.style.display = "none";
     }
-  })
-
-
-  
-
+  });
 });
